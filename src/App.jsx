9b54e6bd1d35d325d81e70e95babbb2be1d28de3
bludbruda1 <g_topeto21@hotmail.com>
2024-07-21 21:22:20 +1200

    Set up basic page template

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./App.css";
+import {
+  AppBar,
+  Card,
+  CardMedia,
+  Container,
+  CssBaseline,
+  Grid,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import SportsMmaIcon from "@mui/icons-material/SportsMma";
+import connor from "./images/connormc.png";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <SportsMmaIcon />
+          <Typography variant="h6">MMA Fighter</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div>
+          <Container maxWidth="md" style={{ marginTop: "100px" }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              MMA Fighter
+            </Typography>
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              gutterBottom
+            >
+              Welcome to MMAFighter, a combat sport simulation game that lets
+              you create your own world of fighting and provides an experience
+              of running the show.
+            </Typography>
+            <div>
+              <Grid container spacing={60} justify="center" gutterTop>
+                <Grid item>
+                  <Paper elevation={3}>
+                    <Card>
+                      <CardMedia
+                        component="img"
+                        height="280"
+                        image={connor}
+                        alt="fighter 1"
+                      />
+                    </Card>
+                  </Paper>
+                </Grid>
+                <Grid item>
+                  <Paper elevation={3}>
+                    <Card>
+                      <CardMedia
+                        component="img"
+                        height="280"
+                        image={connor}
+                        alt="fighter 2"
+                      />
+                    </Card>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
