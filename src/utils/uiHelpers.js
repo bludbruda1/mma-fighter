725bde90c996ef53d424ea5a90eac84abaf5f3ae
BillApes <rowdyroddy90@gmail.com>
2024-11-07 15:20:11 +1300

    dont need this helper

--- src/utils/uiHelpers.js
+++ src/utils/uiHelpers.js
@@ -5,15 +5,3 @@ export const formatFightingStyle = (style) => {
       .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(' ');
   };
-
-// Helper function to properly show in fight position
-export const formatPosition = (position) => {
-  if (!position) return 'Standing';
-  return position
-      .toLowerCase()
-      .split('_')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ')
-      .replace('ground', '')
-      .replace('Position', '');
-};
