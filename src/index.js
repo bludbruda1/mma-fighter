64331e9cc08470587a360859a4a4d35c6f4e9ff7
BillApes <rowdyroddy90@gmail.com>
2025-01-28 16:00:41 +1300

    remove initialise indexDB from app render

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import initDB from "./utils/initDB";
 import { EventProvider } from "./contexts/EventContext";
 import { GameDateProvider } from "./contexts/GameDateContext";
 import { CurrentGameProvider } from "./contexts/CurrentGameContext";
@@ -10,21 +9,16 @@ import { CurrentGameProvider } from "./contexts/CurrentGameContext";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-// Initialize IndexedDB and render the application
-initDB()
-  .then(() => {
-    root.render(
-      <Router>
-        <CurrentGameProvider>
-          <GameDateProvider>
-            <EventProvider>
-              <App />
-            </EventProvider>
-         </GameDateProvider>
-        </CurrentGameProvider>
-      </Router>
-    );
-  })
-  .catch((error) => {
-    console.error("Failed to initialize the database", error);
-  });
+// Render the application
+root.render(
+  <Router>
+    <CurrentGameProvider>
+      <GameDateProvider>
+        <EventProvider>
+          <App />
+        </EventProvider>
+      </GameDateProvider>
+    </CurrentGameProvider>
+  </Router>
+);
+
